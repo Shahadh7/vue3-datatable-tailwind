7deea15ec820fc1d5f2626cdf8c6ebc722383339
shahadh7 <shahadh.hamza@gmail.com>
2023-04-13 05:25:01 +0530

    new updates

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,9 @@
 <script setup>
-import DataTable from './components/DataTable.vue'
-import { ref } from 'vue';
-
-const tableHeads = ref([
-    { name:'id', title: 'id', sort: true },
-    { name:'fname', title: 'First name', sort: false },
-    { name:'lname', title: 'Last name', sort: false },
-    { name:'username', title: 'Username', sort: false },
-]);
-
-const link = ref("https://www.melivecode.com/api/users")
 </script>
 
 <template>
   <main>
-    <DataTable 
-      :table-text-alignment="'text-center'"
-      :data-per-page-option-visible="true"
-      :search-visible="true" 
-      :table-heads="tableHeads"
-      :link="link"
-      >
-        <template #table-data="slotProps">
-          <tr v-if="slotProps.data.length === 0"> No data found</tr>
-          <tr v-for="(user, index) in slotProps.data" :key="index">
-              <td :class="slotProps.css">{{ user.id }}</td>
-              <td :class="slotProps.css">{{ user.fname }}</td>
-              <td :class="slotProps.css">{{ user.lname }}</td>
-              <td :class="slotProps.css">{{ user.username }}</td>
-          </tr>
-        </template>
-    </DataTable>
+    
   </main>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const path = require('path');
+const { defineConfig } = require('vite');
+const vue = require('@vitejs/plugin-vue');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+});
